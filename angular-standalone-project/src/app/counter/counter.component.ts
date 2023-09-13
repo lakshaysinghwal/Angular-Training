396b4691db7b3483698107e0671620d91f6f474f
Lakshay <lakshaysinghwal22@gmail.com>
2023-09-13 17:19:50 +0530

    Adding Angular tarining projects

--- angular-standalone-project/src/app/counter/counter.component.ts
+++ angular-standalone-project/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit, OnDestroy {
   @Input()
   count = 10
 
@@ -24,8 +24,25 @@ export class CounterComponent {
     this.myEvent.emit(this.count)
   }
 
-  //When the life of a component is created
+  //When the life of a component is created(Adding component to a DOM)
   constructor() {
     console.log("Counter component created")
+    // let hobj:any = document.getElementById("hc")
+    // hobj.style.color = "red"
+
+  }
+
+
+  //Before removing component from DOM
+  ngOnDestroy(): void {
+    console.log("Counter component destroyed")
+    
+  }
+
+  //After rendering
+  ngOnInit(): void {
+    console.log("Onit Counter Component")
+    let hobj:any = document.getElementById("hc")
+    hobj.style.color = "red"
   }
 }
